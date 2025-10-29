5e716f5c9d6bebb12e9dd3c1427647c392e314af
Volodymyr Simakov <simakovv@uni-marburg.de>
2025-10-29 20:47:54 +0100

    Implement DB-Manager

--- src/model/Interface.ts
+++
@@ -1,9 +0,0 @@
-import type { Task } from "./Types";
-
-export interface Model {
-    createTask(task: Task): void;
-    readTask(id: string): Task;
-    updateTask(task: Task): void;
-    deleteTask(id: string): void;
-    getTasks(): Task[];
-}
